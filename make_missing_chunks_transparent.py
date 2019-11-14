c4c2e1ef7eea1056b07a3d647239ca20b08c1d01
Tammo Jan Dijkema <T.J.Dijkema@gmail.com>
2019-11-14 14:14:12 +0100

    Add black detection

--- make_missing_chunks_transparent.py
+++ make_missing_chunks_transparent.py
@@ -4,6 +4,12 @@ import numpy as np
 import cv2
 import sys
 
+
+def add_alpha_channel(img):
+    if img.shape[-1] != 3:
+        raise(ValueError("Expecting image with 3 layers (r,g,b), got " + str(img.shape[1])))
+    return np.stack([img[:,:,0], img[:,:,1], img[:,:,2], 0*img[:,:,0]+255], axis=2)
+
 def get_block(img, i):
     """Get a 8x8 block of the image, counting horizontally
     The image has 640/8=80 block horizontally and 480/8=60 blocks vertically
@@ -19,7 +25,7 @@ def get_block(img, i):
     return img[rownum*8:(rownum+1)*8, colnum*8:(colnum+1)*8]
 
 def is_constant(block):
-    return np.all(block == block[0,0])
+    return np.all(block[:,:,:3] == block[1,1,:3])
 
 def is_flagged(img, i):
     """
@@ -29,29 +35,45 @@ def is_flagged(img, i):
     """
     MIN_CHUNK_LENGTH = 12
     block = get_block(img, i)
-    if is_constant(block):
+    if is_constant(block) and np.all(block[1,1,:3]<250):
+        # Heuristic: fully overexposed blocks are not missing chunks
         num_neighborflags = 0
         for neighbor_num in range(max(i-MIN_CHUNK_LENGTH,0), min(i+MIN_CHUNK_LENGTH+1,4799)):
             if neighbor_num == i:
                 continue
             neighbor_block = get_block(img, neighbor_num)
-            if is_constant(neighbor_block) and np.all(neighbor_block[0,0] == block[0,0]):
+            if is_constant(neighbor_block) and np.all(neighbor_block[1,1,:3] == block[1,1,:3]):
                 num_neighborflags += 1
         if num_neighborflags >= MIN_CHUNK_LENGTH:
             return True
     return False
 
 def make_missing_chunks_transparent(img):
-    img2 = np.stack([img[:,:,0], img[:,:,1], img[:,:,2], 0*img[:,:,0]+255], axis=2)
+    if img.shape[-1] != 4:
+        img = add_alpha_channel(img)
 
     for i in range(80*60):
         if is_flagged(img, i):
-            block2 = get_block(img2, i)
-            block2[:,:,3] = 0
+            block = get_block(img, i)
+            block[:,:,3] = 0
 
-    return img2
+    return img
+
+def make_black_blocks_transparent(img):
+    """Makes constant black blocks transparent"""
+    if img.shape[-1] != 4:
+        img = add_alpha_channel(img)
+
+    for i in range(80*60):
+        block = get_block(img, i)
+        if is_constant(block):
+            if np.all(block[:,:,:3] < 4):
+                block[:,:,3] = 0
+
+    return img
 
 if __name__ == "__main__":
     img = cv2.imread(sys.argv[1])
-    img2 = make_missing_chunks_transparent(img)
-    cv2.imwrite(sys.argv[1].rstrip(".jpeg") + ".png", img2)
+    img = make_missing_chunks_transparent(img)
+    img = make_black_blocks_transparent(img)
+    cv2.imwrite(sys.argv[1].rstrip(".jpeg") + ".png", img)
